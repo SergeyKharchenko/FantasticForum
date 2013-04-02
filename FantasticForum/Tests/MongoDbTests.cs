61c0a599a5b5a1987f039ff144bdaef782637651
Sergey Kharchenko <hsvlis4@gmail.com>
2013-04-02 15:01:38 +0300

    same

--- FantasticForum/Tests/MongoDbTests.cs
+++ FantasticForum/Tests/MongoDbTests.cs
@@ -24,6 +24,7 @@ namespace Tests
             collection.Insert(entity);
         }
 
+
         private string GetMongoDbConnectionString()
         {
             return ConfigurationManager.AppSettings.Get("MONGOLAB_URI") ??
