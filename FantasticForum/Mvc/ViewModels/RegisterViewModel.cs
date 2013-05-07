9cc7b804231f61e65b221633cdc6e0a46192e67c
Sergey Kharchenko <hsvlis4@gmail.com>
2013-05-07 23:05:28 +0300

    1

--- FantasticForum/Mvc/ViewModels/RegisterViewModel.cs
+++ FantasticForum/Mvc/ViewModels/RegisterViewModel.cs
@@ -4,12 +4,9 @@ namespace Mvc.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required]
-        [StringLength(30, MinimumLength = 2)]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required]
         [StringLength(30, MinimumLength = 2)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
