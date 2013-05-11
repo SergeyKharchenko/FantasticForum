5c124961cd25bc0af9edaa528f4f995b3b49d9bd
Sergey Kharchenko <hsvlis4@gmail.com>
2013-05-11 17:13:33 +0300

    Rework file picker

---
+++ FantasticForum/Mvc/Scripts/Section/Edit.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $('#btn').click(function (e) {
+        e.preventDefault();
+        $(this).next().click();
+    });
+
+    $('#avatar').change(function () {
+        var file = $(this)[0].files[0];
+        if (file)
+            $('#avatar-name').html(file.name);
+        else
+            $('#avatar-name').html('');
+    });
+});
